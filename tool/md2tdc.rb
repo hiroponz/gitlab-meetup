dae5db337d980ec9d31d30708e5866927c5b8811
Hiroyuki Sato <h-sato@ruby-dev.jp>
2017-01-20 12:53:00 +0900

    Fix bug

--- tool/md2tdc.rb
+++ tool/md2tdc.rb
@@ -1,20 +1,24 @@
 #!/bin/env ruby
 
 ARGF.each do |line|
-  if (match = /\A## (.+)/.match(line))
-    puts "[size=x-large]#{match[1]}[/size]"
-  elsif (match = /\A### (.+)/.match(line))
-    puts "[size=large]#{match[1]}[/size]"
-  elsif (match = /\A#### (.+)/.match(line))
-    puts "[size=medium]#{match[1]}[/size]"
-  elsif (match = /\A( +)(\* .+)/.match(line))
-    prefix = "*" * (match[1].length / 4)
-    puts "#{prefix}#{match[2]}"
-  else
-    if (match = %r|\[(.+)\]\((.+)\)|.match(line))
-      replace = "[url=#{match[2]}]#{match[1]}[/url]"
-      line.sub!(match[0], replace)
+  output =
+    if (match = /\A## (.+)/.match(line))
+      "[size=x-large]#{match[1]}[/size]"
+    elsif (match = /\A### (.+)/.match(line))
+      "[size=large]#{match[1]}[/size]"
+    elsif (match = /\A#### (.+)/.match(line))
+      "[size=medium]#{match[1]}[/size]"
+    elsif (match = /\A( +)(\* .+)/.match(line))
+      prefix = "*" * (match[1].length / 4)
+      "#{prefix}#{match[2]}"
+    else
+      line
     end
-    puts line
+
+  if (match = %r|\[(.+)\]\((.+)\)|.match(output))
+    replace = "[url=#{match[2]}]#{match[1]}[/url]"
+    output.gsub!(match[0], replace)
   end
+
+  puts output
 end
