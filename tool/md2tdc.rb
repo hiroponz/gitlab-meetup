13ea9312999a5edd25e57751477cedf11f472d0d
Hiroyuki Sato <h-sato@ruby-dev.jp>
2017-01-20 12:37:12 +0900

    Add TDC.txt

--- tool/md2tdc.rb
+++ tool/md2tdc.rb
@@ -1,11 +1,20 @@
 #!/bin/env ruby
 
 ARGF.each do |line|
-  if match = /\A## (.+)/.match(line)
+  if (match = /\A## (.+)/.match(line))
     puts "[size=x-large]#{match[1]}[/size]"
-  elsif match = /\A### (.+)/.match(line)
+  elsif (match = /\A### (.+)/.match(line))
     puts "[size=large]#{match[1]}[/size]"
+  elsif (match = /\A#### (.+)/.match(line))
+    puts "[size=medium]#{match[1]}[/size]"
+  elsif (match = /\A( +)(\* .+)/.match(line))
+    prefix = "*" * (match[1].length / 4)
+    puts "#{prefix}#{match[2]}"
   else
+    if (match = %r|\[(.+)\]\((.+)\)|.match(line))
+      replace = "[url=#{match[2]}]#{match[1]}[/url]"
+      line.sub!(match[0], replace)
+    end
     puts line
   end
 end
